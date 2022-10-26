71ced560c74ccc9fbbb0908f7c18f097d0d9568e
unknown <neotaxman007@gmail.com>
2022-10-26 22:27:19 +0100

    Prototype v1.2 swarm-jenkins build

--- db/create.sql
+++ db/create.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS monstertype (
-    mtid INT PRIMARY KEY AUTO_INCREMENT,
-    type VARCHAR(20)
-    fk_nameid INT,
-    FOREIGN KEY(fk_nameid) REFERENCES monstername(id)
+CREATE TABLE IF NOT EXISTS monster_name (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    stars VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS monstername (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50)
-    stars VARCHAR(20)
+CREATE TABLE IF NOT EXISTS monster_type (
+    mtid INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(20),
+    fk_nameid INT,
+    FOREIGN KEY(fk_nameid) REFERENCES monster_name(id)
 );
